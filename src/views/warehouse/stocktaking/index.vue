f3ea392ac19b8acdcc44ecc2cd90422a8dab636c
boduoyejieza <boduoyejieza@gmail.com>
2025-09-01 22:49:48 +0800

    endTime

--- src/views/warehouse/stocktaking/index.vue
+++ src/views/warehouse/stocktaking/index.vue
@@ -282,7 +282,7 @@
               <el-input v-model="scope.row.batchNumber" label-width="200px" placeholder="请输入批号" />
             </template>
           </el-table-column>
-          <el-table-column label="生产日期" prop="batchNo" width="240" show-overflow-tooltip resizable>
+          <el-table-column label="生产日期" prop="beginTime" width="240" show-overflow-tooltip resizable>
             <template slot-scope="scope">
               <el-date-picker clearable
                               v-model="scope.row.beginTime"
@@ -292,10 +292,10 @@
               </el-date-picker>
             </template>
           </el-table-column>
-          <el-table-column label="有效期" prop="batchNo" width="240" show-overflow-tooltip resizable>
+          <el-table-column label="有效期" prop="endTime" width="240" show-overflow-tooltip resizable>
             <template slot-scope="scope">
               <el-date-picker clearable
-                              v-model="scope.row.andTime"
+                              v-model="scope.row.endTime"
                               type="date"
                               value-format="yyyy-MM-dd"
                               placeholder="请选择盘点日期">
