7ba8e9958b4cfde4d2df72dd9c0d0fa7c60ff02d
boduoyejieza <boduoyejieza@gmail.com>
2025-09-05 16:59:39 +0800

    修正合计数量和金额

--- src/views/warehouse/inventory/firstInventory.vue
+++ src/views/warehouse/inventory/firstInventory.vue
@@ -209,7 +209,7 @@ export default {
           return;
         }
         const values = data.map(item => Number(item[column.property]));
-        if(index === 4 || index === 5 || index === 6){
+        if(index === 5 || index === 7){
           if (!values.every(value => isNaN(value))) {
             sums[index] = values.reduce((prev, curr) => {
               const value = Number(curr);
