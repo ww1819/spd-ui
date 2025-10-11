defe0e312891e0cbbce852f32f2280f8ca38ea06
boduoyejieza <boduoyejieza@gmail.com>
2025-10-11 17:37:06 +0800

    出库添加引用科室申请单功能

--- src/components/SelectModel/SelectDApply.vue
+++ src/components/SelectModel/SelectDApply.vue
@@ -56,36 +56,58 @@
         <el-table ref="singleTable" :data="applyList" :row-class-name="applyIndex" @selection-change="handleSelectionChange" height="calc(42vh)" border>
           <el-table-column type="selection" width="55" align="center" />
           <el-table-column label="序号" align="center" prop="index" show-overflow-tooltip resizable />
-          <el-table-column label="仓库" align="center" prop="warehouse.name" width="120" show-overflow-tooltip resizable/>
-<!--          <el-table-column label="耗材" align="center" prop="material.name" width="120" show-overflow-tooltip resizable/>-->
-          <el-table-column label="名称" align="center" prop="material.name" width="180" show-overflow-tooltip resizable/>
-          <el-table-column label="规格" align="center" prop="material.speci" width="180" show-overflow-tooltip resizable/>
-          <el-table-column label="型号" align="center" prop="material.name" width="180" show-overflow-tooltip resizable/>
-          <el-table-column label="单位" align="center" prop="material.fdUnit.unitName" width="180" show-overflow-tooltip resizable/>
-          <el-table-column label="库存数量" align="center" prop="qty" width="80" show-overflow-tooltip resizable/>
-          <el-table-column label="单价" align="center" prop="unitPrice" width="120" show-overflow-tooltip resizable/>
-          <el-table-column label="金额" align="center" prop="amt" width="120" show-overflow-tooltip resizable/>
-          <el-table-column label="批号" align="center" prop="materialNo" width="200" show-overflow-tooltip resizable/>
-          <el-table-column label="有效期" align="center" prop="endTime" width="140" show-overflow-tooltip resizable>
+          <el-table-column label="申领单号" align="center" prop="applyBillNo" show-overflow-tooltip resizable >
             <template slot-scope="scope">
-              <span>{{ parseTime(scope.row.endTime, '{y}-{m}-{d}') }}</span>
+              <el-button type="text" @click="handleView(scope.row)">
+                <span>{{ scope.row.applyBillNo }}</span>
+              </el-button>
             </template>
           </el-table-column>
-          <el-table-column label="生产日期" align="center" prop="beginTime" width="140" show-overflow-tooltip resizable>
+          <el-table-column label="制单日期" align="center" prop="createTime" width="180" show-overflow-tooltip resizable>
             <template slot-scope="scope">
-              <span>{{ parseTime(scope.row.beginTime, '{y}-{m}-{d}') }}</span>
+              <span>{{ parseTime(scope.row.createTime, '{y}-{m}-{d}') }}</span>
             </template>
           </el-table-column>
-          <el-table-column label="批次号" align="center" prop="batchNo" width="200" show-overflow-tooltip resizable/>
-          <el-table-column label="生产厂家" align="center" prop="material.fdFactory.factoryName" width="180" show-overflow-tooltip resizable/>
-          <el-table-column label="供应商" align="center" prop="supplier.name" width="160" show-overflow-tooltip resizable/>
-          <el-table-column label="注册证号" align="center" prop="material.registerNo" width="180" show-overflow-tooltip resizable/>
-          <el-table-column label="包装规格" align="center" prop="material.packageSpeci" width="180" show-overflow-tooltip resizable/>
-          <el-table-column label="库房分类" align="center" prop="material.fdWarehouseCategory.warehouseCategoryName" width="180" show-overflow-tooltip resizable/>
-          <el-table-column label="财务分类" align="center" prop="material.fdFinanceCategory.financeCategoryName" width="180" show-overflow-tooltip resizable/>
-          <el-table-column label="储存方式" align="center" prop="material.isWay" width="180" show-overflow-tooltip resizable>
+          <el-table-column label="仓库" align="center" prop="warehouse.name" show-overflow-tooltip resizable />
+          <el-table-column label="科室" align="center" prop="department.name" show-overflow-tooltip resizable />
+          <el-table-column label="制单人" align="center" prop="createrNmae" show-overflow-tooltip resizable />
+          <el-table-column label="申请状态" align="center" prop="applyBillStatus" show-overflow-tooltip resizable >
             <template slot-scope="scope">
-              <dict-tag :options="dict.type.way_status" :value="scope.row.material.isWay"/>
+              <dict-tag :options="dict.type.biz_status" :value="scope.row.applyBillStatus"/>
+            </template>
+          </el-table-column>
+          <el-table-column label="审核人" align="center" prop="auditPersonName" show-overflow-tooltip resizable />
+          <el-table-column label="审核时间" align="center" prop="auditDate" show-overflow-tooltip resizable>
+            <template slot-scope="scope">
+              <span>{{ parseTime(scope.row.auditDate, '{y}-{m}-{d}') }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="备注" align="center" prop="remark" show-overflow-tooltip resizable />
+          <el-table-column label="操作" align="center" class-name="small-padding fixed-width" show-overflow-tooltip resizable>
+            <template slot-scope="scope">
+              <el-button
+                size="mini"
+                type="text"
+                icon="el-icon-edit"
+                @click="handleUpdate(scope.row)"
+                v-hasPermi="['department:dApply:edit']"
+                v-if="scope.row.applyBillStatus != 2"
+              >修改</el-button>
+              <el-button
+                size="mini"
+                type="text"
+                icon="el-icon-delete"
+                @click="handleDelete(scope.row)"
+                v-hasPermi="['department:dApply:remove']"
+                v-if="scope.row.applyBillStatus != 2"
+              >删除</el-button>
+              <el-button
+                size="mini"
+                type="text"
+                icon="el-icon-view"
+                @click="handleView(scope.row)"
+                v-hasPermi="['department:dApply:view']"
+              >查看</el-button>
             </template>
           </el-table-column>
         </el-table>
