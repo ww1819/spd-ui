2c52707c9148a9b6ef30fac23d173cb9233a62a3
ww1819 <1737914508@qq.com>
2026-08-22 11:11:36 +0800

    style(foundation/material): 将下拉触发方式从点击改为悬停
    优化物料页面操作列下拉菜单的交互体验，改为hover触发更符合常规使用习惯

--- src/views/foundation/material/index.vue
+++ src/views/foundation/material/index.vue
@@ -364,7 +364,7 @@
       <el-table-column label="操作" align="center" header-align="center" class-name="material-action-col material-col-center small-padding fixed-width" width="100">
         <template slot-scope="scope">
           <el-dropdown
-            trigger="click"
+            trigger="hover"
             class="material-row-ops"
             @command="(cmd) => handleRowOpsCommand(cmd, scope.row)"
           >
